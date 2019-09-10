c2fbe3fbf0325069d4e8c7772950da4ad2ebc60e
Muthukumar Thangavinayagam <2063172+mkpracticalworks@users.noreply.github.com>
2019-09-10 23:09:39 +0530

    Hibernate 4 SaveOrUpdate example.

---
+++ dn-hibernate4-gradle-example/src/main/java/dn/hibernate4/gradle/example/DnHibernate4SaveOrUpdateExample.java
@@ -0,0 +1,35 @@
+package dn.hibernate4.gradle.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import dn.hibernate4.gradle.example.model.DnPersonDetail;
+import dn.hibernate4.gradle.example.util.DnHibernateUtil;
+
+/**
+ * @author Muthukumar Thangavinayagam
+ *
+ */
+public class DnHibernate4SaveOrUpdateExample {
+	public static void main(String[] args) {
+		Session session = null; //
+		try {
+			SessionFactory sessionFactory = DnHibernateUtil.getSessionFactory();
+			session = sessionFactory.openSession();
+			Transaction transaction = session.beginTransaction();
+			DnPersonDetail person = (DnPersonDetail)session.load(DnPersonDetail.class, Long.valueOf(1));
+			System.out.println(" Person Name :"+person.getName()+" Age"+person.getAge()+" ContactNumber: "+person.getContactNumber()+" "+person.getAddress());
+			
+			person.setAddress("Bangalore, India");
+			
+			session.saveOrUpdate(person);
+			transaction.commit();
+			
+		} catch (Exception e) {
+			System.out.println("Err msg ..."+e);
+		} finally {
+			session.close();
+		}
+	}
+}
